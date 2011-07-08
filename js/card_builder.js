7e1b35cc654db31bfd35d934e693b1b9777cf780
Zoran <kokovic.zoran@gmail.com>
2011-07-08 15:45:24 +0200

    Optimized Card builder

--- js/card_builder.js
+++ js/card_builder.js
@@ -1,56 +1,49 @@
+// Grid dimensions
+var size_per_level = [4,6,8,10];
 
-// Grid dimension for levels 1 - 5
-var size_per_level = [ 4, 6, 8, 10, 12];
+var $pane = $("#card_container");
 
-var card_count = 16;
+var gsize = 4;
 
-build_board = function(level, is_empty){
-	row_counter = 1;
-	col_counter = 1;
-	if(is_empty==true){
-		grid_size = size_per_level[3];		
-	} 
-	else {
-		grid_size = size_per_level[level-1];	
-	}
-	card_count = grid_size * grid_size / 2;
-	while(row_counter <= grid_size){
+var ccoun = 0;
+
+var cblog = function(message) {
+	$("#my_log_div").prepend(" cb: " + message + " ");
+};
+
+var card = function(dim) {
+	return ("<div class='column grid_" + dim + "'></div>");
+};
+
+precalculate = function(level) {
+	gsize = size_per_level[level-1];
+	ccoun = gsize * gsize / 2;
+};
+
+build_board = function(level, is_empty) {
+	precalculate(level);
+	for (var i=1; i <= gsize; i++) {
 		row = "<div class='row'>";
-		while(col_counter <= grid_size){	
-			elem_id = row_counter + "_" + col_counter;			
-			image_id = "image_" + ((row_counter - 1) * grid_size + col_counter);
-			row = append_card(row, elem_id, image_id, is_empty);
-			col_counter = col_counter + 1;
-		}
-		if(grid_size < 10){
-		  row = row + "<div class='column grid_" + (10-grid_size)+"'></div>";
-		}
-		row = row + "</div>";
-		$("#card_container").append(row);
-		col_counter = 1;
-		row_counter = row_counter + 1;
-	}
+		for (var j=1; j<=gsize; j++) {	
+			image_id = "image_" + ((i)+"_"+(j));
+			row = placement(row, image_id, is_empty);
+		};
+		$pane.append(row + "</div>");
+	};
 };
 
-append_card = function(row, elem_id, image_id, is_empty){
+placement = function(row, image_id, is_empty) {
 	var content = [];
-	content[0] = "<div id='" + elem_id + "' class='column grid_1'>";
+	content[0] = "<div id='dvv_" + image_id + "' class='column grid_1'>";
 	content[1] = "<ul class='hover_block'><li id='li_" + image_id + "' class='hover_action'>";
-	content[2] = "<a id='" + image_id + "' href='#'>";
-	content[3] = "<img id='qm_li_" + image_id + "' class='questionMark'  style='position:absolute;z-index: 2;' alt='UWP' src='res/";
-	if(is_empty==true) {
-		content[4] =  "card_back.png"
-	} 
-	else {
-		content[4] = "question_mark_48.jpg"
+	content[2] = "<a id='" + image_id + "' class='user_photo'  href='#' src=''>";
+	content[3] = "<img id='qm_li_" + image_id + "' class='questionMark' alt='UWP' src='res/";
+	content[4] = (is_empty==true) ? "card_back.png" : "question_mark_48.jpg";
+	content[5] = "'>";
+	content[6] = "</a></li></ul></div>";
+	row = row + content.join('');
+	if(gsize < 10) {
+		row = row + card(10-gsize);
 	}
-	content[5] = "'>"; 
-	content[6] = "</a></li><ul></ul></ul></div>";
-	return (row + content.join(''));
-};
-
-append_row_start = function(){
-	$("<div>")
-	  	.attr("class", "row")
-		.appendTo("#card_container");
+	return row;
 };
